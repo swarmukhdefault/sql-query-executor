c154bbce132b3d50b88ef94edda5163c700041d8
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2023-12-23 14:55:24 +0530

    chore: Remove unneeded hooks

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useDebounce = <T>(value: T, delayInMs: number): T => {
   const [data, setData] = useState(() => value);
@@ -12,67 +12,4 @@ export const useDebounce = <T>(value: T, delayInMs: number): T => {
   }, [value]);
 
   return data;
-};
-
-interface Dimension {
-  height: number;
-  width: number;
-}
-
-export const useWindowResizeHandler = (): Dimension => {
-  const [dimensions, setDimensions] = useState<Dimension>({
-    height: window.innerHeight,
-    width: window.innerWidth
-  });
-  const debouncedDimensions = useDebounce(dimensions, 900);
-
-  useEffect(() => {
-    const resizeListener = (): void => {
-      setDimensions({ height: window.innerHeight, width: window.innerWidth });
-    };
-
-    window.addEventListener('resize', resizeListener);
-
-    return () => window.removeEventListener('resize', resizeListener);
-  }, []);
-
-  return debouncedDimensions;
-};
-
-interface CountdownTimer {
-  timerStatus: 'PRISTINE' | 'RUNNING' | 'STOPPED';
-  value: number;
-  startTimer(): void;
-  stopTimer(): void;
-}
-export const useCountdownTimer = (duration: number): CountdownTimer => {
-  const [value, setValue] = useState(() => duration);
-  const [status, setStatus] = useState<CountdownTimer['timerStatus']>('PRISTINE');
-
-  const timer = useRef<NodeJS.Timer>();
-
-  const start = useCallback(() => {
-    if (!timer.current) {
-      setStatus('RUNNING');
-      timer.current = setInterval(() => {
-        setValue((timerValue) => (timerValue === 0 ? 0 : timerValue - 1));
-      }, 1000);
-    }
-  }, []);
-
-  const stop = useCallback(() => {
-    if (timer.current) {
-      clearInterval(timer.current);
-      timer.current = undefined;
-      setValue(duration);
-      setStatus('STOPPED');
-    }
-  }, []);
-
-  return {
-    value,
-    timerStatus: status,
-    startTimer: start,
-    stopTimer: stop
-  };
 };
