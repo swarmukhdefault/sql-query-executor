bd5b54ce1f3140baa6324ea077fd9ac3dd5f2150
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2023-12-23 14:54:58 +0530

    feat: Create a rudimentary SQL parser and show data from appropriate data-source

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,60 @@
 import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 
+import { stringQueryParser } from '@utils/helpers';
+import MockServer from '@services/MockServer';
+
 import '@assets/styles/App.scss';
 
 const App: FunctionComponent = () => {
   const [query, setQuery] = useState('');
+  const [records, setRecords] = useState([]);
 
   const updateQuery = (e: ChangeEvent<HTMLTextAreaElement>): void => setQuery(e.target.value);
+
+  const doExecuteQuery = (): void => {
+    try {
+      const p = stringQueryParser(query);
+      MockServer.getRecords(p.dataSource, p.fields).then(setRecords);
+    } catch (e) {
+      // TODO: display error
+    }
+  };
 
   return (
     <>
       <h1>SQL Query Executor</h1>
       <div>
         <div id='query-input-form'>
-          <label htmlFor='input-query'>Query</label>
+          <label htmlFor='input-query'>Query (case-insensitive)</label>
           <textarea id='input-query' onChange={updateQuery}>
             {query}
           </textarea>
         </div>
-        <button>Execute</button>
+        <button onClick={doExecuteQuery}>Execute</button>
       </div>
       <div className='container'>
-        <table>
-          <thead>
-            <tr>
-              <th>employeeID</th>
-              <th>lastName</th>
-              <th>firstName</th>
-              <th>title</th>
-              <th>titleOfCourtesy</th>
-              <th>birthDate</th>
-              <th>hireDate</th>
-              <th>address</th>
-              <th>city</th>
-              <th>region</th>
-              <th>postalCode</th>
-              <th>country</th>
-              <th>homePhone</th>
-              <th>extension</th>
-              <th>photo</th>
-              <th>notes</th>
-              <th>reportsTo</th>
-              <th>photoPath</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Davolio</td>
-              <td>Nancy</td>
-              <td>Sales Representative</td>
-              <td>Ms.</td>
-              <td>1948-12-08 00:00:00.000</td>
-              <td>1992-05-01 00:00:00.000</td>
-              <td>507 20th Ave. E. Apt. 2A</td>
-              <td>Seattle</td>
-              <td>WA</td>
-              <td>98122</td>
-              <td>USA</td>
-              <td>(206) 555-9857</td>
-              <td>5467</td>
-              <td>
-                0x151C2F00020000000D000E0014002100FFFFFFFF4269746D617020496D616765005061696E742E506963747572650001050000020000000700000050427275736800000000000000000020540000424D20540000000000007600000028000000C0000000DF0000000100040000000000A0530000CE0E0000D80E0000000000
-              </td>
-              <td>
-                Education includes a BA in psychology from Colorado State University in 1970. She also completed The Art
-                of the Cold Call. Nancy is a member of Toastmasters International.
-              </td>
-              <td>2</td>
-              <td>http://accweb/emmployees/davolio.bmp</td>
-            </tr>
-          </tbody>
-        </table>
+        {records.length === 0 ? (
+          <div>No data!</div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                {Object.keys(records[0]).map((key: string, idx: number) => (
+                  <th key={idx}>{key}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record, idx: number) => (
+                <tr key={idx}>
+                  {Object.values(record).map((value: any, idx2: number) => (
+                    <td key={idx2}>{value}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </>
   );
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,2 +1,21 @@
-export const hello = (): string => 'Hello';
-export const world = (): string => 'Bingo';
+import { DataSource, ParsedDataSource } from '@utils/models';
+
+const SELECT_ALL_REGEX = /SELECT \* FROM ([\w]+)/i;
+
+export const stringQueryParser = (query: string): ParsedDataSource => {
+  if (SELECT_ALL_REGEX.test(query)) {
+    const parsedAttributes = SELECT_ALL_REGEX.exec(query)!;
+    const dataSource = DataSource[parsedAttributes[1].toLocaleUpperCase() as keyof typeof DataSource];
+
+    if (dataSource) {
+      return {
+        dataSource,
+        fields: 'all'
+      };
+    } else {
+      throw new Error('Invalid DataSource');
+    }
+  } else {
+    throw new Error('Invalid expression');
+  }
+};
