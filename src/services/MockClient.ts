d045c233081259093ef49b4b56977a2bdcd2921f
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2023-12-23 15:53:33 +0530

    feat: Add more data-sources

--- src/services/MockClient.ts
+++ src/services/MockClient.ts
@@ -1,9 +1,17 @@
 import { DataSource } from '@utils/models';
 
 import categories from './stubs/categories.json';
+import customers from './stubs/customers.json';
+import employees from './stubs/employees.json';
+import products from './stubs/products.json';
+import suppliers from './stubs/suppliers.json';
 
 const DATA: Record<keyof typeof DataSource, any> = {
-  CATEGORIES: categories
+  CATEGORIES: categories,
+  CUSTOMERS: customers,
+  EMPLOYEES: employees,
+  PRODUCTS: products,
+  SUPPLIERS: suppliers
 };
 
 const MockClient = {
